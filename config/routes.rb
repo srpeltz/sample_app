31854fb7590ace035af53b4869b2d6b5d55c008f
steven peltzer <srpeltzer@yahoo.com>
2017-02-19 22:24:47 -0800

    Finish static pages (except root route)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
+  get  'static_pages/home'
+  get  'static_pages/help'
+  get  'static_pages/about'
+  get  'static_pages/contact'
 
-  get 'static_pages/help'
-
-  root "application#hello"
 
 
 
